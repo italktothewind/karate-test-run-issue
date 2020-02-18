8319bed0e99fe79b12402f6bd9ddf2b368d0e76e
italktothewind <luis.brassara@almundo.com>
2020-02-17 23:32:08 -0300

    ExampleIT runner

--- src/it/java/ExampleIT.java
+++ src/it/java/ExampleIT.java
@@ -1,10 +1,8 @@
-
-import com.intuit.karate.junit4.Karate;
 import example.ExampleRunner;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Karate.class)
-@SuiteClasses({ ExampleRunner.class })
+@RunWith(Suite.class)
+@SuiteClasses(ExampleRunner.class)
 public class ExampleIT {}
